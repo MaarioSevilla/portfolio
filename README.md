94056119ac6b93e711a5ae3ae0960c3159387ca5
Mario Sevilla <4174254-MaarioSevilla@users.noreply.gitlab.com>
2021-04-19 00:38:39 -0500

    avance

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # portfolio
+
+Este es mi portafolio, te encuentras en la parte del codigo, si deceas ver un previsualizacion por favor utiliza el enlace siguiente:
+
+Gracias!
---
+++ src/js/master.js
@@ -0,0 +1,7 @@
+const navIcon = document.querySelector(".nav-icon");
+const nav = document.querySelector("nav");
+
+navIcon.onclick = function () {
+    //nav.classList.toggle('nav-blur');
+    nav.classList.toggle('show');
+}
